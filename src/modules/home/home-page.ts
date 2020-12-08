599705f84ea2fa94998470902d8d3c70646b111a
Andreas Wänqvist <andrkarl@gmail.com>
2020-12-09 00:34:44 +0100

    feat: swapping front page image

--- src/modules/home/home-page.ts
+++ src/modules/home/home-page.ts
@@ -1,15 +1,14 @@
 export class HomePage {
   public imgSource = "";
 
-  private visitors: number = 487984567;
   private bb8Media = "./../../../content/images/legends/BB-8.png";
   private bb9eMedia = "./../../../content/images/legends/BB-9E.png";
 
   constructor() {
-    this.imgSource = this.getImageSource(this.visitors);
+    this.imgSource = this.getImageSource();
   }
 
-  private getImageSource(visitorCounter): string {
-    return visitorCounter % 2 === 0 ? this.bb8Media : this.bb9eMedia;
+  private getImageSource(): string {
+    return new Date().getTime() % 2 === 0 ? this.bb8Media : this.bb9eMedia;
   }
 }
