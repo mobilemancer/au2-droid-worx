f58c4de942eb6fad6f2149485c45f61ee39c0f67
Andreas Wänqvist <andrkarl@gmail.com>
2021-01-05 22:42:31 +0100

    wip: styling menu

--- src/components/nav-menu/nav-menu.ts
+++ src/components/nav-menu/nav-menu.ts
@@ -1,9 +1,19 @@
 import { IEventAggregator, EventAggregator } from "aurelia";
 
 export class NavMenu {
+  public menu: HTMLElement;
+
   constructor(@IEventAggregator private eventAggregator: EventAggregator) {}
 
-  public toggleShoppingCart() {
+  public toggleShoppingCart(): void {
     this.eventAggregator.publish("toggle-cart");
+  }
+
+  public toggleMenu(): void {
+    if (this.menu.style.display === "none" || this.menu.style.display === "") {
+      this.menu.style.display = "block";
+    } else {
+      this.menu.style.display = "none";
+    }
   }
 }
